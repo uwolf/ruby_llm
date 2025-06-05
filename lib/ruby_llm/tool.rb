68b809b0d730a048990488d54f95fffcc10d6998
Eric Guo <eric@cloud-mes.com>
2025-06-05 18:08:23 +0800

    Fix Encoding::CompatibilityError: Unicode Normalization not appropriate for ASCII-8BIT (#218)
    Encoding::CompatibilityError: Unicode Normalization not appropriate for
    ASCII-8BIT is introduced after #215, commit:
    d34592efe00697457ac020cd5ec80a13c2827fed
    
    The fix is copy from:
    https://github.com/knu/ruby-domain_name/pull/27
    
    ---------
    
    Co-authored-by: Carmine Paolino <carmine@paolino.me>

--- lib/ruby_llm/tool.rb
+++ lib/ruby_llm/tool.rb
@@ -49,14 +49,14 @@ module RubyLLM
     end
 
     def name
-      self.class.name
-          .unicode_normalize(:nfkd)
-          .encode('ASCII', replace: '')
-          .gsub(/[^a-zA-Z0-9_-]/, '-')
-          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-          .downcase
-          .delete_suffix('_tool')
+      klass_name = self.class.name
+      normalized = klass_name.to_s.dup.force_encoding('UTF-8').unicode_normalize(:nfkd)
+      normalized.encode('ASCII', replace: '')
+                .gsub(/[^a-zA-Z0-9_-]/, '-')
+                .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+                .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+                .downcase
+                .delete_suffix('_tool')
     end
 
     def description
--- spec/ruby_llm/tool_spec.rb
+++ spec/ruby_llm/tool_spec.rb
@@ -32,5 +32,13 @@ RSpec.describe RubyLLM::Tool do
       stub_const('TestModule::SampleTool', Class.new(described_class))
       expect(TestModule::SampleTool.new.name).to eq('test_module--sample')
     end
+
+    it 'handles ASCII-8BIT encoded class names without raising Encoding::CompatibilityError' do
+      # This simulates a class name that is ASCII-8BIT encoded
+      tool_class = Class.new(described_class)
+      ascii_8bit_name = 'SampleTool'.dup.force_encoding('ASCII-8BIT')
+      allow(tool_class).to receive(:name).and_return(ascii_8bit_name)
+      expect(tool_class.new.name).to eq('sample')
+    end
   end
 end
